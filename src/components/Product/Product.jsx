24f7624c9083b73e25c7077fe6fc9bec5fe7f141
olehhapuk <oleggapuk20@gmail.com>
2023-04-20 20:13:55 +0300

    css modules

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,27 +1,13 @@
-import './Product.css';
-
-// const user = { username: 'john', password: '12345678' };
-// // const username = user.username;
-// // const password = user.password;
-// const { username, password } = user;
-
-// console.log(username);
+import styles from './Product.module.css';
 
 function Product({ imageUrl, name, price, available }) {
-  const nameClasses = ['product__name'];
-  if (available) {
-    nameClasses.push('product__name_available');
-  } else {
-    nameClasses.push('product__name_unavailable');
-  }
-
-  console.log(nameClasses);
-
   return (
     <div>
       <img src={imageUrl} width="200" />
-      <h3 className={nameClasses.join(' ')}>{name}</h3>
-      <p>Price: {price}$</p>
+      <h3 className={available ? styles.nameAvailable : styles.nameUnavailable}>
+        {name}
+      </h3>
+      <p className={styles.price}>Price: {price}$</p>
       {available ? <button>Купити</button> : <p>Товар не доступний</p>}
     </div>
   );
