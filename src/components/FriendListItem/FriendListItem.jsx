536204d2ba0f65554a5c3e16982201cc2820fc23
olehhapuk <oleggapuk20@gmail.com>
2023-04-25 20:46:14 +0300

    centered friend item

--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -2,7 +2,7 @@ import styles from './FriendListItem.module.css';
 
 function FriendListItem({ avatar, name, isOnline }) {
   return (
-    <li className="item">
+    <li className={styles.item}>
       <span className={isOnline ? styles.online : styles.offline}></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
