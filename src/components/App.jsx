690e34170d5c2759f36f3de4e42520c13c196575
olehhapuk <oleggapuk20@gmail.com>
2023-04-25 20:31:48 +0300

    transaction history, friend status

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,14 @@
 import FriendList from './FriendList/FriendList';
 import friends from './FriendList/friends.json';
+import TransactionHistory from './TransactionHistory/TransactionHistory';
+import transactions from './TransactionHistory/transactions.json';
 
 function App() {
   return (
     <div>
-      <FriendList friends={friends} />
       {/* <Profile username={user.name} /> */}
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} />
     </div>
   );
 }
--- src/components/FriendListItem/FriendListItem.jsx
+++ src/components/FriendListItem/FriendListItem.jsx
@@ -1,7 +1,9 @@
+import styles from './FriendListItem.module.css';
+
 function FriendListItem({ avatar, name, isOnline }) {
   return (
     <li className="item">
-      <span className="status"></span>
+      <span className={isOnline ? styles.online : styles.offline}></span>
       <img className="avatar" src={avatar} alt="User avatar" width="48" />
       <p className="name">{name}</p>
     </li>
