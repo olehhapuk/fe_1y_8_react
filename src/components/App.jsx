818b02f1b2cfdb4ebfb72ccc189f515df857dcf1
olehhapuk <oleggapuk20@gmail.com>
2023-04-29 20:53:22 +0300

    setup

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,28 +1,5 @@
-import Counter from './Counter';
-import Switch from './Switch';
-
 function App() {
-  function handleClick1(e) {
-    console.log(e.target);
-  }
-
-  function handleButtonClick(a = 'hello') {
-    console.log(a);
-  }
-
-  return (
-    <div>
-      <Switch>Content 1</Switch>
-      <hr />
-      <Counter initialValue={1} step={5} />
-      <hr />
-
-      <button onClick={handleClick1}>Click me with event</button>
-      <button onClick={() => handleButtonClick('abc')}>
-        Click me with params
-      </button>
-    </div>
-  );
+  return <div>App</div>;
 }
 
 export default App;
