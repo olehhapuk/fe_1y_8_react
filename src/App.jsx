b9dde24ec1c26e5a4a303bc285f0f0f7e646b08a
olehhapuk <oleggapuk20@gmail.com>
2023-06-13 20:40:02 +0300

    load more

--- src/App.jsx
+++ src/App.jsx
@@ -7,28 +7,49 @@ import { searchArticles } from './services/articlesService';
 
 function App() {
   const [articles, setArticles] = useState([]);
-  const [query, setQuery] = useState('react');
+  const [query, setQuery] = useState('');
   const [loading, setLoading] = useState(false);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(0);
+  const [pagesCount, setPagesCount] = useState(0);
 
   useEffect(() => {
+    if (query === '') {
+      return;
+    }
+
     setLoading(true);
     setError(null);
 
-    searchArticles(query, 0)
+    searchArticles(query, page)
       .then((data) => {
-        setArticles(data.hits);
+        setArticles((prevArticles) => [...prevArticles, ...data.hits]); // ... - spread оператор - розмазує всі елементи в новий масив
+        setPagesCount(data.nbPages);
       })
       .catch((err) => setError(err.message))
       .finally(() => setLoading(false));
-  }, [query]);
+  }, [query, page]);
+
+  function loadMore() {
+    setPage((prevPage) => prevPage + 1);
+  }
+
+  function search(newQuery) {
+    setQuery(newQuery);
+    setPage(0);
+    setArticles([]);
+  }
 
   return (
     <div>
-      <Searchbar onSearch={setQuery} />
+      <Searchbar onSearch={search} />
       {error && <p>{error}</p>}
       <RotatingLines width={50} visible={loading} />
       <ArticlesList articles={articles} />
+
+      {page < pagesCount && !error && (
+        <button onClick={loadMore}>Load more</button>
+      )}
     </div>
   );
 }
