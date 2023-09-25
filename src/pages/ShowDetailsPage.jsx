2d1b7c82612637800f26fe2e7617a3380ef00fbb
olehhapuk <oleggapuk20@gmail.com>
2023-09-25 23:08:32 +0300

    show details

--- src/pages/ShowDetailsPage.jsx
+++ src/pages/ShowDetailsPage.jsx
@@ -1,18 +1,37 @@
 import { useParams, Link, Outlet } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getShowDetails } from '../services/showsService';
 
 function ShowDetailsPage() {
   const { showId } = useParams();
 
+  const [show, setShow] = useState(null);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getShowDetails(showId)
+      .then((res) => setShow(res))
+      .finally(() => setIsLoading(false));
+  }, [showId]);
+
   return (
     <div>
-      <p>ShowDetailsPage #{showId}</p>
-
-      <Link to={`/shows/${showId}`}>Reviews</Link>
-      <br />
-      <Link to="cast">Cast</Link>
-      <br />
+      {isLoading && <p>Loading...</p>}
+      {show && (
+        <div>
+          <h1>{show.title}</h1>
+          <p>{show.tagline}</p>
+          <hr />
+          {show.genres.map((genre) => (
+            <span key={genre}>{genre} </span>
+          ))}
+          <p>{show.description}</p>
+        </div>
+      )}
+      <Link to={`/shows/${showId}`}>Reviews</Link> <Link to="cast">Cast</Link>{' '}
       <Link to="episodes">Episodes</Link>
-
       <Outlet />
     </div>
   );
--- src/pages/ShowsPage.jsx
+++ src/pages/ShowsPage.jsx
@@ -1,12 +1,32 @@
 import { Link } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { searchShowsService } from '../services/showsService';
 
 function ShowsPage() {
+  const [shows, setShows] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    searchShowsService('', 1)
+      .then((res) => setShows(res))
+      .finally(() => setIsLoading(false));
+  }, []);
+
   return (
     <div>
-      <p>ShowsPage</p>
-      <Link to="/shows/1">Show 1 details</Link>
-      <br />
-      <Link to="/shows/2">Show 2 details</Link>
+      <p>Shows Page</p>
+
+      {isLoading && <p>Loading...</p>}
+
+      <ul>
+        {shows.map((show) => (
+          <li key={show.id}>
+            <Link to={`/shows/${show.id}`}>{show.title}</Link>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
