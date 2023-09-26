f75e86f15f065776882804247e5209be3d282761
olehhapuk <oleggapuk20@gmail.com>
2023-09-26 21:00:04 +0300

    cast and reviews page

--- src/pages/ShowCastPage.jsx
+++ src/pages/ShowCastPage.jsx
@@ -1,5 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getShowCast } from '../services/showsService';
+
 function ShowCastPage() {
-  return <div>ShowCastPage</div>;
+  const { showId } = useParams();
+
+  const [cast, setCast] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getShowCast(showId)
+      .then((res) => setCast(res))
+      .finally(() => setIsLoading(false));
+  }, [showId]);
+
+  return (
+    <div>
+      <h3>Cast:</h3>
+      {isLoading && <p>Loading...</p>}
+
+      <ul>
+        {cast.map((person) => (
+          <li key={person.id}>
+            <h4>{person.name}</h4>
+            <p>{person.bio}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ShowCastPage;
--- src/pages/ShowReviewsPage.jsx
+++ src/pages/ShowReviewsPage.jsx
@@ -1,5 +1,36 @@
+import { useParams } from 'react-router-dom';
+import { useState, useEffect } from 'react';
+import { getShowReviews } from '../services/showsService';
+
 function ShowReviewsPage() {
-  return <div>ShowReviewsPage</div>;
+  const { showId } = useParams();
+
+  const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setIsLoading(true);
+
+    getShowReviews(showId)
+      .then((res) => setReviews(res))
+      .finally(() => setIsLoading(false));
+  }, [showId]);
+
+  return (
+    <div>
+      <h3>Reviews:</h3>
+      {isLoading && <p>Loading...</p>}
+
+      <ul>
+        {reviews.map((review) => (
+          <li key={review.id}>
+            <h4>{review.author.name}</h4>
+            <p>{review.text}</p>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
 
 export default ShowReviewsPage;
