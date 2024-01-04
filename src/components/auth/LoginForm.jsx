3329c8b3df23cae8ad0c9385bf16ec232dc54975
olehhapuk <oleggapuk20@gmail.com>
2024-01-04 20:35:03 +0200

    login

---
+++ src/components/auth/LoginForm.jsx
@@ -0,0 +1,97 @@
+import {
+  Stack,
+  Button,
+  FormControl,
+  FormLabel,
+  FormErrorMessage,
+  Input,
+  Wrap,
+  Alert,
+  AlertIcon,
+} from '@chakra-ui/react';
+import { Link } from 'react-router-dom';
+import { useFormik } from 'formik';
+import * as yup from 'yup';
+import { useDispatch } from 'react-redux';
+import { loginAction } from '../../redux/auth/authActions';
+import { loginService } from '../../services/authServices';
+import { useState } from 'react';
+
+const validationSchema = yup.object().shape({
+  username: yup
+    .string()
+    .min(3)
+    .max(50, 'Username cannot be longer than 50')
+    .required('Username is required'),
+  password: yup.string().min(8).max(255).required('Password is required'),
+});
+
+function LoginForm() {
+  const dispatch = useDispatch();
+
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const formik = useFormik({
+    initialValues: {
+      username: '',
+      password: '',
+    },
+    validationSchema,
+    onSubmit: (values) => {
+      setIsLoading(true);
+      setError(null);
+
+      loginService(values)
+        .then((data) => {
+          dispatch(loginAction(data));
+        })
+        .catch((err) => setError(err.response.data.message))
+        .finally(() => setIsLoading(false));
+    },
+  });
+
+  return (
+    <Stack as="form" onSubmit={formik.handleSubmit}>
+      <FormControl
+        isRequired
+        isInvalid={!!formik.errors.username && formik.touched.username}
+      >
+        <FormLabel>Username</FormLabel>
+        <Input placeholder="Username" {...formik.getFieldProps('username')} />
+        <FormErrorMessage>{formik.errors.username}</FormErrorMessage>
+      </FormControl>
+
+      <FormControl
+        isRequired
+        isInvalid={!!formik.errors.password && formik.touched.password}
+      >
+        <FormLabel>Password</FormLabel>
+        <Input
+          placeholder="Password"
+          type="password"
+          {...formik.getFieldProps('password')}
+        />
+        <FormErrorMessage>{formik.errors.password}</FormErrorMessage>
+      </FormControl>
+
+      {error && (
+        <Alert status="error">
+          <AlertIcon />
+          {error}
+        </Alert>
+      )}
+
+      <Wrap>
+        <Button colorScheme="blue" type="submit" isLoading={isLoading}>
+          Login
+        </Button>
+        <Button as={Link} to="/register" variant="ghost">
+          Register
+        </Button>
+      </Wrap>
+    </Stack>
+  );
+}
+
+export default LoginForm;
--- src/pages/auth/LoginPage.jsx
+++ src/pages/auth/LoginPage.jsx
@@ -1,5 +1,16 @@
+import { Heading, Stack } from '@chakra-ui/react';
+import LoginForm from '../../components/auth/LoginForm';
+
 function LoginPage() {
-  return <div>LoginPage</div>;
+  return (
+    <Stack>
+      <Heading as="h3" size="lg">
+        Login
+      </Heading>
+
+      <LoginForm />
+    </Stack>
+  );
 }
 
 export default LoginPage;
