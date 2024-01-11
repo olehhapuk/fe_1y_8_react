100049aa4eba99176898717040a39ae96d9a0110
olehhapuk <oleggapuk20@gmail.com>
2024-01-11 22:43:41 +0200

    root layout

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,10 +1,36 @@
-import { Container } from '@chakra-ui/react';
+import { Box, Container, Flex } from '@chakra-ui/react';
 import { Outlet } from 'react-router-dom';
 
 function RootLayout() {
   return (
     <Container maxWidth="container.xl">
-      <Outlet />
+      <Flex>
+        <Box
+          width="300px"
+          height="100vh"
+          flexShrink={0}
+          position="sticky"
+          top="0"
+          borderRightWidth="1px"
+        >
+          Left Sidebar
+        </Box>
+
+        <Box flexGrow={1}>
+          <Outlet />
+        </Box>
+
+        <Box
+          width="300px"
+          height="100vh"
+          flexShrink={0}
+          position="sticky"
+          top="0"
+          borderLeftWidth="1px"
+        >
+          Right Sidebar
+        </Box>
+      </Flex>
     </Container>
   );
 }
